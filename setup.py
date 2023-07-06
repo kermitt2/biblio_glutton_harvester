784e27e23033a902acf080d28914da0f5f9c8bd2
lopez <patrice.lopez@science-miner.com>
2023-07-06 21:27:37 +0200

    increase version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='biblio_glutton_harvester', version='0.2.1', packages=find_packages(), url='https://github.com/kermitt2/biblio_glutton_harvester', license='Apache-2.0')
+setup(name='biblio_glutton_harvester', version='0.2.2', packages=find_packages(), url='https://github.com/kermitt2/biblio_glutton_harvester', license='Apache-2.0')
