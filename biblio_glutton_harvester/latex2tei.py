3929461614b94a1418a762602994bcd9917d5e30
lopez <patrice.lopez@science-miner.com>
2023-07-26 12:20:53 +0200

    improve latexml speed

--- biblio_glutton_harvester/latex2tei.py
+++ biblio_glutton_harvester/latex2tei.py
@@ -9,6 +9,7 @@ import time
 from biblio_glutton_harvester.OAHarvester import generateStoragePath, _load_config, _serialize_pickle, _deserialize_pickle
 import zipfile
 import lmdb
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 # logging
 import logging
@@ -57,6 +58,12 @@ class LaTeX2tei(object):
         apply the LaTeXML transformation to TEI XML. Rename the zip file to explicitely indicate that
         we have latex sources in it. 
         """
+        batch_size = 20
+
+        # for batch processing, store the latex zip archive file and zip parent directories 
+        the_files = []
+        the_roots = []
+
         # walk through the data directory, copy .nxml files to the temp directory
         for root, dirs, files in os.walk(self.config["data_path"]):
             for the_file in files:
@@ -68,62 +75,69 @@ class LaTeX2tei(object):
                     if not force and os.path.isfile(os.path.join(root,identifier+".latex.tei.xml")):
                         continue
 
-                    # unzip this thing, this is the easiest way to manage then latex files
-                    directory_to_extract_to = os.path.join(self.config["data_path"], the_file.replace(".zip", "_zip_tmp"))
-                    try:
-                        os.mkdir(directory_to_extract_to)
-                        with zipfile.ZipFile(os.path.join(root,the_file), 'r') as zip_ref:
-                            zip_ref.extractall(directory_to_extract_to)
-                        # examine tex files and find root latex file
-                        latex_files = []
-                        for root2, dirs2, files2 in os.walk(directory_to_extract_to):
-                            for the_file2 in files2:
-                                # normally all NLM/JATS files are stored with extension .nxml, but for safety we also cover .nlm extension
-                                if the_file2.endswith(".tex"):
-                                    latex_files.append(os.path.join(root2,the_file2))
+                    if len(the_files) >= batch_size:
+                        self.process_batch(the_files, the_roots)
 
-                        status_result = "fail"
-                        if the_file.endswith(".latex.zip"):
-                            latex_tei_file = os.path.join(root, the_file.replace(".zip", ".tei.xml"))
-                        else:
-                            latex_tei_file = os.path.join(root, the_file.replace(".zip", ".latex.tei.xml"))
-                        if len(latex_files) == 1:
-                            # easy case, just one latex file 
-                            status_result = self.latexml2tei(latex_files[0], directory_to_extract_to, latex_tei_file)
-                            #print(latex_files[0], status_result)
-                        else:
-                            # examine the latex files to find the root
-                            root_file = self.find_root_latex(latex_files)
-                            if root_file != None:
-                                status_result = self.latexml2tei(root_file, directory_to_extract_to, latex_tei_file)
-                                #print(root_file, status_result)
-                        if status_result == "success":
-                            # update catalog following outcome of the conversion
-                            local_entry = None
-                            update_entry = False
-                            '''
-                            with self.env.begin() as txn:
-                                local_object = txn.get(id_document.encode(encoding='UTF-8'))
-                                if local_object != None:
-                                    local_entry = _deserialize_pickle(local_object)
-                                    if local_entry != None:
-                                        local_entry["resources"]["latex"] = True
-                                        update_entry = True
-                                        if "resources" in local_entry and not "tei" in local_entry["resources"]:
-                                            local_entry["resources"]["tei"] = True
-                            if update_entry and local_entry != None:
-                                with self.env.begin(write=True) as txn:
-                                    txn.put(id_document.encode(encoding='UTF-8'), _serialize_pickle(local_entry))
-                            '''
-                    except:
-                        logging.exception("Cannot unzip the archive file: " + os.path.join(root,the_file))
-                    finally:
-                        # delete extraction directory
-                        shutil.rmtree(directory_to_extract_to)
+                    the_files.append(the_file)
+                    the_roots.append(root)
 
-                        # rename zip file if latex sources are indeed there
-                        if not the_file.endswith(".latex.zip"):
-                            os.rename(os.path.join(root,the_file), os.path.join(root,the_file.replace(".zip", ".latex.zip")))
+                    #self.process_archive_file(the_file, root=root)
+
+        # process last batch if not empty
+        if len(the_files) >= 0:
+            self.process_batch(the_files, the_roots)
+
+    def process_batch(self, the_files, the_roots):
+        with ProcessPoolExecutor(max_workers=8) as executor:
+            results = executor.map(self.process_archive_file, the_files, the_roots)#, timeout=120)
+
+    def process_archive_file(self, zip_file, root=None):
+        '''
+        Examine a latex zip source file and process the main latex file
+        '''
+        # unzip this zip thing, this is the easiest way to manage then latex files
+        if root == None:
+            root = self.config["data_path"]
+        directory_to_extract_to = os.path.join(self.config["data_path"], zip_file.replace(".zip", "_zip_tmp"))
+        try:
+            os.mkdir(directory_to_extract_to)
+            with zipfile.ZipFile(os.path.join(root,zip_file), 'r') as zip_ref:
+                zip_ref.extractall(directory_to_extract_to)
+            # examine tex files and find root latex file
+            latex_files = []
+            for root2, dirs2, files2 in os.walk(directory_to_extract_to):
+                for the_file2 in files2:
+                    if the_file2.endswith(".tex"):
+                        latex_files.append(os.path.join(root2,the_file2))
+
+            status_result = "fail"
+            if zip_file.endswith(".latex.zip"):
+                latex_tei_file = os.path.join(root, zip_file.replace(".zip", ".tei.xml"))
+            else:
+                latex_tei_file = os.path.join(root, zip_file.replace(".zip", ".latex.tei.xml"))
+            if len(latex_files) == 1:
+                # easy case, just one latex file 
+                status_result = self.latexml2tei(latex_files[0], directory_to_extract_to, latex_tei_file)
+                #print(latex_files[0], status_result)
+            else:
+                # examine the latex files to find the root
+                root_file = _find_root_latex(latex_files)
+                if root_file != None:
+                    status_result = self.latexml2tei(root_file, directory_to_extract_to, latex_tei_file)
+                    #print(root_file, status_result)
+            if status_result == "success":
+                # update catalog following outcome of the conversion
+                local_entry = None
+                update_entry = False
+        except:
+            logging.exception("Cannot unzip the archive file: " + os.path.join(root,zip_file))
+        finally:
+            # delete extraction directory
+            shutil.rmtree(directory_to_extract_to)
+
+            # rename zip file if latex sources are indeed there
+            if not zip_file.endswith(".latex.zip"):
+                os.rename(os.path.join(root,zip_file), os.path.join(root,zip_file.replace(".zip", ".latex.zip")))
 
     def latexml2tei(self, root_latex_file, directory_to_extract_to, tei_destination):
         #print("LaTeXML2tei", root_latex_file, directory_to_extract_to, tei_destination)
@@ -153,17 +167,22 @@ class LaTeX2tei(object):
                 result = error['message']
             else:
                 result = e.returncode
+        finally:
+            # clear the latexml scories
+            for the_file in os.listdir("."):
+                if the_file.endswith(".log") and the_file != "harvested.log":
+                    os.remove(the_file)
 
         return result
 
-    def find_root_latex(self, latex_file_lists):
-        for latex_file in latex_file_lists:
-            with open(latex_file) as file:
-                for line in file:
-                    if line.startswith("\\document"):
-                        # it should be enough for \documentclass and \documentstyle
-                        return latex_file
-        return latex_file_lists[0]
+def _find_root_latex(latex_file_lists):
+    for latex_file in latex_file_lists:
+        with open(latex_file) as file:
+            for line in file:
+                if line.startswith("\\document"):
+                    # it should be enough for \documentclass and \documentstyle
+                    return latex_file
+    return latex_file_lists[0]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Converter LaTeX document to TEI")
@@ -177,5 +196,5 @@ if __name__ == "__main__":
     config_path = args.config
     force = args.force
     
-    nlm2tei = LaTeX2tei(config_path=config_path)
-    nlm2tei.process(force=force)
+    latex2tei = LaTeX2tei(config_path=config_path)
+    latex2tei.process(force=force)
